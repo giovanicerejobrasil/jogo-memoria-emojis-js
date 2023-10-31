3bfa85a1399b90c57b95fb36748fee2659f74085
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2023-10-31 16:38:49 -0300

    Update engine.js

---
+++ src/scripts/engine.js
@@ -0,0 +1,42 @@
+const emojis = [
+  "😍",
+  "😍",
+  "😪",
+  "😪",
+  "🙈",
+  "🙈",
+  "🦄",
+  "🦄",
+  "🎈",
+  "🎈",
+  "🍜",
+  "🍜",
+  "💵",
+  "💵",
+  "🥨",
+  "🥨",
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
+  document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
+
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
+
+function checkMatch() {}
